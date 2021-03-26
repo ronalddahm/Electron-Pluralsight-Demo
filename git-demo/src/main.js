a96f73a7d028830e465e8065d4c963df2215619a
Ronald <ronald.dahm.larsen@lego.com>
2021-03-26 14:25:35 +0100

    git demo 2

--- git-demo/src/main.js
+++ git-demo/src/main.js
@@ -1,6 +1,19 @@
 const electron = require('electron');
-const {app} = electron;
+const {app, BrowserWindow} = electron;
+
+let mainWindow
 
 app.on('ready', _ => {
     console.log('Ready');
+
+    mainWindow = new BrowserWindow( {
+        width:400,
+        height: 100
+    });
+
+    mainWindow.loadURL(`file:&//${__dirname}/status.html`);
 });
+
+app.on('close', _ => {
+    mainWindow = null;
+})
---
+++ git-demo/src/status.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
+const os = require('os')
+
+let timer
+
+function isDir(dir)
+{
+    try {
+        console.log('Check dir:', dir)
+        return fs.lstatSync(dir).isDirectory();
+    } catch (error) {
+        return false;        
+    }
+}
+
+function checkGitStatus(dir) {
+    exec('git status', {
+        cwd: dir
+    }, (err, stdout,stderr) => {
+       console.log("Err", err);
+       console.log("stdout", stdout);
+       console.log('stderr', stderr);
+
+       if ( err) return setStatus('unknown');
+       if ( /nothing to commit/.test(stdout)) return setStatus('clean');
+       return setStatus('dirty');
+    })
+}
+
+function formatDir(dir) {
+    return /^~/.test(dir) ? os.homedir() + dir.substr(1).trim() : dir.trim();
+}
+
+function removeStatus() {
+    const el = document.getElementById('status');
+    el.classList.remove('unknow', 'clean', 'dirty');
+    return el;
+}
+
+function setStatus(status) {
+    const el = removeStatus();
+
+    el.classList.add(status);
+}
+
+document.getElementById('input').addEventListener('keyup', evt => {
+    clearTimeout(timer);
+    timer = setTimeout( _=> {
+        const dir = formatDir(evt.target.value);
+        if ( isDir(dir)) {
+            checkGitStatus(dir);
+        } else {
+            console.log('not dir');
+        }
+    },500);
+})
